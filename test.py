6b813a3e15f9f08701b6d1de82ccda298040e9fb
LuckyKolo <Lebrono098>
2021-09-01 22:44:12 +0200

    add edits

--- test.py
+++ test.py
@@ -9,20 +9,66 @@ root = Tk()
 root.title("Music Player")
 root.geometry('800x400')
 
-def volume(val):
-	volume = int(val) / 100
+songsframe = LabelFrame(root,text="Song Playlist",font=("times new roman",15,"bold"),bg="grey",fg="white",bd=5,relief=GROOVE)
+songsframe.place(x=20, y=20,width=400,height=200)
+scrol_y = Scrollbar(songsframe,orient=VERTICAL)
+playlist = Listbox(songsframe,yscrollcommand=scrol_y.set,selectbackground="gold",selectmode=SINGLE,font=("times new roman",12,"bold"),bg="silver",fg="navyblue",bd=5,relief=GROOVE)
+scrol_y.pack(side=RIGHT,fill=Y)
+scrol_y.config(command=playlist.yview)
+playlist.pack(fill=BOTH)
+os.chdir("songs")
+songtracks = os.listdir()
+
+tracks = []
+for track in songtracks:
+	playlist.insert(END,track)
+	tracks.append(track)
+
+
+def play():
 	mixer.init()
-	mixer.music.set_volume(volume)
+	mixer.music.load(playlist.get(ACTIVE))
+	mixer.music.play()
 
 
-frame = LabelFrame(root, text='Volume')
-frame.grid(row=5, column=0, padx=50, pady=300)
+Play = Button(root, text="Play", command=play)
+Play.grid()
+Play.place(x=160,y=368, width=57)
 
 
-scale = Scale(frame, from_=0, to=100, orient=HORIZONTAL, command=volume)
-scale.set(50)
-scale.pack()
 
+
+#edits all
+def Previous():
+    #to get the selected song index
+    previous_one=songs_list.curselection()
+    #to get the previous song index
+    previous_one=previous_one[0]-1
+    #to get the previous song
+    temp2=songs_list.get(previous_one)
+                   temp2=f'C:/Users/DataFlair/python-mp3-music-player/{temp2}'
+    mixer.music.load(temp2)
+    mixer.music.play()
+    songs_list.selection_clear(0,END)
+    #activate new song
+    songs_list.activate(previous_one)
+    #set the next song
+    songs_list.selection_set(previous_one)
+def Next():
+    #to get the selected song index
+    next_one=songs_list.curselection()
+    #to get the next song index
+    next_one=next_one[0]+1
+    #to get the next song
+    temp=songs_list.get(next_one)
+            temp=f'C:/Users/DataFlair/python-mp3-music-player/{temp}'
+    mixer.music.load(temp)
+    mixer.music.play()
+    songs_list.selection_clear(0,END)
+    #activate newsong
+    songs_list.activate(next_one)
+     #set the next song
+    songs_list.selection_set(next_one)
 
 
 root.mainloop()
