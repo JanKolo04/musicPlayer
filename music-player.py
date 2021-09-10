c1cb15afe5936318a0b13eeb89170b4499410ade
Jan Kołodziej <76879087+JanKolo04@users.noreply.github.com>
2021-09-11 00:45:45 +0200

    exit button

--- music-player.py
+++ music-player.py
@@ -9,7 +9,7 @@ from mutagen.mp3 import MP3
 
 root = Tk()
 root.title("Music Player")
-root.geometry('440x450')
+root.geometry('440x500')
 root.resizable(0,0)
 root.config(background='black')
 
@@ -41,12 +41,21 @@ resized_next = next_image.resize((40,45), Image.ANTIALIAS)
 next_pick = ImageTk.PhotoImage(resized_next)
 
 
-#button on previous button
+#photo on previous button
 previous_image = Image.open('images/previous.png')
 
 resized_previous = previous_image.resize((40,40), Image.ANTIALIAS)
 
 previous_pick = ImageTk.PhotoImage(resized_previous)
+
+
+
+#button on previou button
+exit_image = Image.open('images/exit.png')
+
+resized_exit = exit_image.resize((45,40), Image.ANTIALIAS)
+
+exit_pick = ImageTk.PhotoImage(resized_exit)
 
 
 #function to load button
@@ -65,6 +74,13 @@ Load = Button(root, text="Load", command=load)
 Load.pack()
 Load.place(x=10,y=10);
 '''
+
+#exit button
+exit = Button(root, text='Exit',  image=exit_pick, bd=0,command=root.destroy)
+exit.grid()
+exit.place(x=380, y=10, height=40, width=40)
+
+
 
 #grab song lenght time info
 def play_time():
@@ -95,7 +111,7 @@ def volume(val):
 
 
 frame = LabelFrame(root, text='Volume', cursor='target')
-frame.grid(row=5, column=0, padx=165, pady=250)
+frame.grid(row=5, column=0, padx=165, pady=290)
 
 
 scale = Scale(frame, from_=0, to=100, orient=HORIZONTAL, command=volume, length=100)
@@ -107,13 +123,13 @@ scale.pack()
 #bottom belt
 down_belt = Label(root, bg='#424242')
 down_belt.grid()
-down_belt.place(x=0, y=390, height=60, width=440)
+down_belt.place(x=0, y=440, height=60, width=440)
 
 
 
 #function to play button
 songsframe = LabelFrame(root,text="Song Playlist",font=("times new roman",15,"bold"),bg="grey",fg="white",bd=5,relief=GROOVE)
-songsframe.place(x=20, y=20,width=400,height=200)
+songsframe.place(x=20, y=70,width=400,height=200)
 scrol_y = Scrollbar(songsframe,orient=VERTICAL)
 playlist = Listbox(songsframe,yscrollcommand=scrol_y.set,selectbackground="gold",selectmode=SINGLE,font=("times new roman",12,"bold"),bg="grey",fg="navyblue",bd=5,relief=GROOVE)
 scrol_y.pack(side=RIGHT,fill=Y)
@@ -139,7 +155,7 @@ def play():
 
 Play = Button(root, image=play_pick, borderwidth=0, command=play)
 Play.grid()
-Play.place(x=160,y=403, height=34, width=48)
+Play.place(x=160,y=453, height=34, width=48)
 
 #function to pause button
 playing_state = False 
@@ -156,7 +172,7 @@ def pause():
 
 Pause = Button(root, image=pause_pick, borderwidth=0, command=pause)
 Pause.grid()
-Pause.place(x=240,y=403, height=34, width=48)
+Pause.place(x=240,y=453, height=34, width=48)
 
 
 def next_button():
@@ -176,7 +192,7 @@ def next_button():
 
 Next = Button(root, image=next_pick, borderwidth=0, command=next_button)
 Next.grid()
-Next.place(x=320,y=400, height=40, width=40)
+Next.place(x=320,y=450, height=40, width=40)
 
 
 
@@ -197,14 +213,14 @@ def previous_button():
 
 Previous = Button(root, image=previous_pick, borderwidth=0, command=previous_button)
 Previous.grid()
-Previous.place(x=80,y=400, height=40, width=40)
+Previous.place(x=80,y=450, height=40, width=40)
 
 
 
 #status bar
-status_bar = Label(root, text='', bg="black", fg="white", bd=0, relief=GROOVE, anchor=SW)
+status_bar = Label(root, text='', bg="black", fg="white", bd=1, relief=GROOVE, anchor=SW)
 status_bar.grid()
-status_bar.place(height=60, width=440, y=330)
+status_bar.place(height=60, width=440, y=380)
 
 
 
