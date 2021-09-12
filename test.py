3917cd53ff007eb2c115933a5617b1269db4703a
Jan Kołodziej <76879087+JanKolo04@users.noreply.github.com>
2021-09-13 00:28:30 +0200

    okej

--- test.py
+++ test.py
@@ -1,7 +1,15 @@
-from tkinter import *
+from tkinter import * 
+import tkinter.ttk as ttk
+from tkmacosx import *
 
 root = Tk()
-root.minsize(290, 700)
-root.title("My Window")
-root.wm_iconbitmap('images/exit.png')
+root.geometry('290x100')
+root.config(background='black')
+
+scale = ttk.Scale(root, from_=0, to=100, length=100, orient=HORIZONTAL)
+scale.pack()
+
+style = ttk.Style()
+style.configure('Horizontal.TScale', bd=0, background='black')
+
 root.mainloop()
